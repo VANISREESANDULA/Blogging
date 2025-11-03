fdd72e75c52de92144294abad78563cb662c16b6
VANISREESANDULA <sandulavanisree@gmail.com>
2025-11-03 19:12:14 +0530

    registration page updated

--- src/component/Registrationpage.jsx
+++ src/component/Registrationpage.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Registrationpage = () => {
   return (
-    <div>Registrationpage</div>
+    <form action="">
+    <h1>New User Registration </h1>
+    <label htmlFor="">Name</label><br /><br />
+    <input type="text" placeholder='Enter Your Name' /> <br /><br />
+    <label htmlFor="">Email</label><br /><br />
+    <input type="email" placeholder='Enter Your Email' /> <br /><br />
+    <label htmlFor="">Create Password</label><br /><br />
+    <input type="password" placeholder='Enter Password' /><br /><br />
+    <label htmlFor="">Confirm Password</label><br /><br />
+    <input type="password" placeholder='Enter Password' /><br /><br />
+    <button></button>
+   </form>
   )
 }
 
